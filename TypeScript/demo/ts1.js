a9ae95651cf4a609af9bf7c074690770e64dbb42
AjiMIde <chenjitao@phone580.com>
2019-08-09 17:57:13 +0800

    update typescript basic type

---
+++ TypeScript/demo/ts1.js
@@ -0,0 +1,5 @@
+var x = 4;
+x = '4';
+x = false;
+var y = [4, '4', false];
+y[3] = { z: 'zz' };
---
+++ TypeScript/demo/ts2.ts
@@ -0,0 +1,21 @@
+let name: string = 'aji'
+let age: number = 18
+let sex: string = 'man'
+let isEarthMan: boolean = true
+
+let favorites: Array<string> = ['singing', 'swimming', 'eating']
+let favorites2: string[] = ['coding', 'watching']
+
+enum BodyWeightLevel {light, middle, heavy}
+let bodyWeight: BodyWeightLevel = BodyWeightLevel.middle
+
+
+let sign = `I like ${favorites[0]}`
+
+
+
+let aji: Object = {
+    name: 'aji',
+    age: 18,
+    sex: 'man'
+}
